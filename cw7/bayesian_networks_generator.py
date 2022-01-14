4690d95dccd3260b2e95974c05794041809ce520
Konrad Wojda <konrad2001@outlook.com>
2022-01-14 19:02:15 +0100

    Added loading from json and sample json structure

--- cw7/bayesian_networks_generator.py
+++ cw7/bayesian_networks_generator.py
@@ -1,3 +1,4 @@
+import json
 import random
 from typing import Counter
 
@@ -6,6 +7,35 @@ class Node:
         self.name = name
         self.dependents = dependents if dependents else set()
         self.prob_dict = prob_dict if prob_dict else dict()
+
+
+def load_from_json(path):
+    with open(path, 'r') as fh:
+        data = json.load(fh)
+    nodes = []
+    for item in data:
+        name = item["name"]
+        dependents = item["dependents"]
+        probabilities = item["prob_dict"]
+        prob_dict = {}
+        if len(probabilities) == 1:
+            key = True if list(probabilities.keys())[0] else False
+            prob_dict[key] = list(probabilities.values())[0]
+        else:
+            for booleans_str, prob in probabilities.items():
+                booleans_str = booleans_str.strip(',')
+                values = booleans_str.split(", ")
+                booleans = []
+                for elem in values:
+                    if elem == 'True':
+                        booleans.append(True)
+                    else:
+                        booleans.append(False)
+                booleans = tuple(booleans)
+                prob_dict[booleans] = prob
+        nodes.append(Node(name, dependents, prob_dict))
+    return nodes
+
 
 
 def generate_sample(nodes):
@@ -31,18 +61,19 @@ def generate_sample(nodes):
 
 
 if __name__ == "__main__":
-    nodes = [
-        Node("Chair", prob_dict={True: 0.8}),
-        Node("Sport", prob_dict={True: 0.02}),
-        Node("Back", dependents=["Chair", "Sport"], prob_dict={
-            (True, True): 0.9,
-            (True, False): 0.2,
-            (False, True): 0.9,
-            (False, False): 0.01
-        }),
-        Node("Ache", dependents=["Back"], prob_dict={(True,): 0.7, (False,): 0.1})
-    ]
+    # nodes = [
+    #     Node("Chair", prob_dict={True: 0.8}),
+    #     Node("Sport", prob_dict={True: 0.02}),
+    #     Node("Back", dependents=["Chair", "Sport"], prob_dict={
+    #         (True, True): 0.9,
+    #         (True, False): 0.2,
+    #         (False, True): 0.9,
+    #         (False, False): 0.01
+    #     }),
+    #     Node("Ache", dependents=["Back"], prob_dict={(True,): 0.7, (False,): 0.1})
+    # ]
 
+    nodes = load_from_json("cw7/nodes_structure.json")
     samples = [generate_sample(nodes) for _ in range(100)]
     for sample in samples:
         print(sample)
